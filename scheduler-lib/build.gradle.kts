2b7473263a54ae8a3fe64aad37918ad6ab7feb93
MMmM <mm@MMs-MacBook-Pro.local>
2025-11-22 01:55:03 +0330

    Fix JitPack build: remove local java.home and update to Java 11

--- scheduler-lib/build.gradle.kts
+++ scheduler-lib/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     publishing {
         singleVariant("release") {
@@ -56,4 +56,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
 }
